e8434a23fec3878c2f293c8f057f0b43f2c9834b
Ariel Garmendia <arl@travelgosystems.com>
2022-05-10 20:41:29 +0200

    ErrorLogger

---
+++ Website/Controllers/ShowErrorsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Exceptional;
+using System.Threading.Tasks;
+
+namespace Mabui.Finance.Controllers
+{
+    public class ShowErrorsController : Controller
+    {
+        public async Task Exceptions()
+        {
+            await ExceptionalMiddleware.HandleRequestAsync(HttpContext).ConfigureAwait(false);
+        }
+    }
+}

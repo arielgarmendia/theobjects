f460f0c1eb559e05ce77014b7378b9941405b447
arielgarmendia <arl_garm@hotmail.com>
2022-08-02 13:30:38 +0200

    stripe callback test

---
+++ WebAPI/Controllers/StripeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Inventory.WebAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.WebAPI.Controllers
+{
+    [ApiController]
+    public class StripeController : Controller
+    {
+        private IHostingEnvironment _env;
+
+        public StripeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        // GET api/products/all
+        [HttpGet]
+        [Route("api/stripe/callback")]
+        public ActionResult<StripeResult> Callback(string code = "test code", string state = "test state")
+        {
+            try
+            {
+                return new StripeResult(){ Code = code, State = state };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ WebAPI/Model/StripeResult.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory.WebAPI.Model
+{
+    public class StripeResult
+    {
+        public string Code { get; set; }
+        public string State { get; set; }
+    }
+}
